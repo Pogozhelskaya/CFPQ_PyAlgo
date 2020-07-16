a8993bbd8151c4b90a5e0a34f579734bfd466fef
Pogozhelskaya Vlada <pogozhelskaya@gmail.com>
2020-07-16 21:52:53 +0700

    Fixes

--- src/algo/matrix_base.py
+++ src/algo/matrix_base.py
@@ -1,10 +1,6 @@
-from pygraphblas import binaryop, Matrix
-
 from src.grammar.cnf_grammar import CnfGrammar
 from src.graph.label_graph import LabelGraph
 
-from pygraphblas.demo.gviz import draw, draw_op
-from pygraphblas import *
 
 def matrix_base_algo(g: LabelGraph, grammar: CnfGrammar):
     m = LabelGraph()
@@ -15,8 +11,6 @@ def matrix_base_algo(g: LabelGraph, grammar: CnfGrammar):
         for l, r1, r2 in grammar.complex_rules:
             old_nnz = m[l].nvals
             m[l] += m[r1] @ m[r2]
-           # dg = draw_op(m[r1], '@', m[r2],m[r1] @ m[r2] )
-           # dg.view()
             new_nnz = m[l].nvals
             changed = not old_nnz == new_nnz
     return m
