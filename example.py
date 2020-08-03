fba5bc901143e7d52b4141e5bb86c31db4c518db
Pogozhelskaya Vlada <pogozhelskaya@gmail.com>
2020-08-03 17:04:03 +0700

    bug fixes

--- example.py
+++ example.py
@@ -2,7 +2,8 @@ from src.grammar.cnf_grammar import CnfGrammar
 from src.graph.label_graph import LabelGraph
 from src.utils.time_profiler import SimpleTimer
 from src.algo.matrix_base import matrix_base_algo
-from src.algo.single_source.single_source import SingleSourceAlgoBrute, SingleSourceAlgoSmart
+from src.algo.single_source.single_source import \
+    SingleSourceAlgoBrute, SingleSourceAlgoSmart, SingleSourceAlgoOpt
 
 g = LabelGraph.from_txt('deps/CFPQ_Data/data/WorstCase/Matrices/worstcase_128.txt')
 gr = CnfGrammar.from_cnf('deps/CFPQ_Data/data/WorstCase/Grammars/Brackets.cnf')
@@ -28,5 +29,18 @@ for i in sources_vertices:
 
 print(sum)
 
-assert m[gr.start_nonterm].to_lists() == m1.to_lists(), 'Not equal!'
-assert m[gr.start_nonterm].to_lists() == m2.to_lists(), 'Not equal!'
+ss_ao = SingleSourceAlgoOpt(g, gr)
+
+sum = 0
+for i in sources_vertices:
+    st = SimpleTimer()
+    st.tic()
+    m3 = ss_ao.solve([i])
+    st.toc()
+    sum += st.duration
+
+print(sum)
+
+assert m.to_lists() == m1.to_lists(), 'Not equal!'
+assert m.to_lists() == m2.to_lists(), 'Not equal!'
+assert m.to_lists() == m3.to_lists(), 'Not equal!'
